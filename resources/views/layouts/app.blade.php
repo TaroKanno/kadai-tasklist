8959e8ad820f04fb6fd6b0c4df1e3ca446afd4e9
Taro Kanno <taro1225.enjoy@icloud.com>
2017-12-26 09:44:03 +0000

    CRUD tasks

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta chrarset="utf-8">
+        <title>TaskList</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
